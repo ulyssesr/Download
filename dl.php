f36171bcb5c051c7b392fba53e9e6fb1fd90b847
Ulysses <ulyssesr@yahoo.com>
2016-02-12 13:58:08 -0800

    fix formatting

--- dl.php
+++ dl.php
@@ -1,6 +1,8 @@
-<?php $filename = $_GET['file'];
-$filepath = "./";
-$url = $filepath.$filename;
+<?php 
+$filename	= $_GET['file'];
+$filepath	= "./";
+$url			= $filepath.$filename;
 header("Content-disposition: attachment; filename=".$filename."");
 header("Content-type: application/octet-stream");
-readfile("".$url."");?>
+readfile("".$url."");
+/* end of file */
